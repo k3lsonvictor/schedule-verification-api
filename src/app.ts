657026042ca3900a46678fc0703d3e16a5bda8ba
drogariagloboti <drogariaglobo.ti@gmail.com>
2025-04-29 14:11:50 -0300

    feat: update user email in users.json and add CORS middleware in app.ts

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,12 @@ import { scheduleVerification } from "./services/schedules";
 const app = express();
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use("/users", userRoutes);
 
