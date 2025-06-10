f336060c4065eb448f7e94124be66ee260294e43
k3lsonvictor <k3lson.victor@gmail.com>
2025-06-10 00:47:22 -0300

    fix: simplify CORS configuration to allow all origins

--- src/app.ts
+++ src/app.ts
@@ -3,21 +3,19 @@ import bodyParser from "body-parser";
 import userRoutes from "./routes/user-routes";
 import { scheduleVerification } from "./services/schedules";
 import cors from "cors";
-import { connectDB } from "./config/db";
 
 const app = express();
-connectDB();
-
-const allowedOrigin = "https://verificacao-de-agendamento-e3lce.ondigitalocean.app"
-
-app.use(cors({
-  origin: allowedOrigin,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}));
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use(cors({ origin: "*" }));
+
 app.use("/users", userRoutes);
 
 app.listen(3000, () => {
