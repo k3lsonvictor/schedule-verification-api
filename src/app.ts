b4f3472f8ae6f8c6afae6ba8f9a4485b53d06741
k3lsonvictor <k3lson.victor@gmail.com>
2025-06-10 00:09:38 -0300

    fix: update server port from 3000 to 8000 in app configuration

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ app.use(cors({
 app.use(bodyParser.json());
 app.use("/users", userRoutes);
 
-app.listen(3000, () => {
+app.listen(8000, () => {
   console.log("Server is running on port 3000");
   scheduleVerification();
 })
